a311d43f897aa6d63db4d67af100ba09313013bc
Marina Budanko <barbaric1marina@gmail.com>
2024-05-02 10:37:03 +0200

    Navbar bez routera

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import Navbar from "./Navbar"
+import Pricing from "./pages/Pricing"
+import About from "./pages/About"
+import Home from "./pages/Home"
 
 function App() {
-  return <Navbar />
+  let component
+  switch(window.location.pathname){
+    case "/":
+      component=<Home />
+      break
+    case "/pricing":
+      component = <Pricing />
+      break
+    case "/about":
+      component= <About />
+      break
+      
+  }
+  return (
+    <><Navbar />
+    <div className="container">{component}</div>
+    </>
+  
+  ) 
 }
 
 export default App
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,19 @@
 export default function Navbar(){
-    return <nav className="nav">
+      return <nav className="nav">
+        
         <a href="/" className="site-title">Site Name</a>
         <ul>
-            <li>
-                <a href="/pricing">Pricing</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+            <CustomLink href="/pricing">Pricing</CustomLink>
+            <CustomLink href="/about">About</CustomLink>
         </ul>
 
     </nav>
+function CustomLink ({href,children, ...props}){
+    const path = window.location.pathname
+    return (
+        <li className={path === href ? "active" :""}>
+            <a href={href} {...props}>{children}</a>
+        </li>
+    )
+}
 }
